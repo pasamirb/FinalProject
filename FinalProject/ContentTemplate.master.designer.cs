a4fa0ad3dd4ece2b6222dc6213d8f663c5b1802e
Samir Bharatbhai Patel <pasamirb@shernet.sheridancollege.ca>
2019-03-30 20:05:12 -0400

    Database created and chatview form is created

--- FinalProject/ContentTemplate.master.designer.cs
+++ FinalProject/ContentTemplate.master.designer.cs
@@ -13,6 +13,15 @@ namespace FinalProject {
     public partial class NestedMasterPage1 {
         
         /// <summary>
+        /// head control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder head;
+        
+        /// <summary>
         /// PageTitle control.
         /// </summary>
         /// <remarks>
